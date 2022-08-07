bf59d23abc4346ad0486e2736a0e957834382835
NataliaOrlova <natakarelskaya@yandex.ru>
2022-08-07 16:55:21 +0400

    Добавили решение задачи 3

--- task3_EvenNumbers/Program.cs
+++ task3_EvenNumbers/Program.cs
@@ -1,4 +1,35 @@
-﻿//Напишите программу, которая на вход принимает число (N), 
-//а на выходе показывает все чётные числа от 1 до N.
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+Console.Write($"Четные числа из ряда от 1 до {num}:  ");
+while (count <= num)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write($" {count} ");
+    }
+   count++;
+}
+
+
+
+// или 2-й вариант:
+
+
+// Console.WriteLine("Введите натуральное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int count = 0;
+// Console.Write($"Четные числа из ряда от 1 до {num}:  ");
+// while (count < num - 1)
+
+// {
+//     count += 2;
+//     Console.Write($" {count} ");
+// }
 
 

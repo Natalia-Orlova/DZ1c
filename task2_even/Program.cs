5f5b57b479485c400545203cb3b6f084861568d9
NataliaOrlova <natakarelskaya@yandex.ru>
2022-08-07 15:58:12 +0400

    Добавили решение задачи 2

--- task2_even/Program.cs
+++ task2_even/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine($"Число {num} является четным");
+}
+else 
+{
+    Console.WriteLine($"Число {num} не является четным");
+}
